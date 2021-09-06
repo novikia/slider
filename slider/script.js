79f23bbe480b9439871c4536e26aa1ce5f5f8561
Igor Novik <igor.novik@amobee.com>
2021-09-06 23:33:29 +0300

    endless slider

--- slider/script.js
+++ slider/script.js
@@ -1,6 +1,7 @@
 const next = document.querySelector('.next'),
       back = document.querySelector('.back'),
       slides = document.querySelectorAll('.slide'),
+      slideElements = document.getElementsByClassName('smallslide');
       points = document.querySelectorAll('.point'),
       line = document.querySelector('.slider-line');
 
@@ -66,16 +67,33 @@ points.forEach((item, indexItem) => {
 let position = 0;
 function lineLeft() {
     position--;
-    console.log(position);
-    line.style.transform = `translateX(${position * 33.5}%`;
+    if (position < 0) {
+        position += slideElements.length;
+    }
+    //line.style.transform = `translateX(${position * 33.5}%`;
+    moveLine(position);
 }
 
 function lineRight() {
     position++;
     console.log(position);
-    line.style.transform = `translateX(${position * 33.5}%`;
+    //line.style.transform = `translateX(${position * 33.5}%`;
+    moveLine(position);
 }
+    moveLine(position);
 
-
-
+    function moveLine(position){
+    for (var i = 0; i < slideElements.length; i++) {
+        var order = ((i + position + 1) % slideElements.length) - 1;
+        
+        if (order >= 0 & order <= 2){
+            slideElements[i].classList.remove('hidden');
+        }
+        else {
+            slideElements[i].classList.add('hidden');
+        }
+        var elemPosition = order - i;
+        slideElements[i].style.transform = `translateX(${elemPosition * 200 + 2}px)`;
+    }
+}
 
